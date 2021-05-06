21aaf34dc719c3dd76a60fc5730b4c758297a873
Nils Funk <nilsiism>
2021-05-06 13:35:14 +0100

    Clean up main application

--- se_app/src/main_tsdf.cpp
+++ se_app/src/main_tsdf.cpp
@@ -12,39 +12,52 @@
 #include "draw.h"
 #include "reader.hpp"
 
-static std::ostream* log_stream;
-static std::ofstream log_file_stream;
-
-int main() {
+int main()
+{
+  // Setup log stream
+  std::ofstream log_file_stream;
   log_file_stream.open("./log.txt");
-  log_stream = &log_file_stream;
   se::perfstats.setFilestream(&log_file_stream);
 
-
-  // MILESTONE 01
-  // Creating a single-res TSDF map with colour and 4 voxel block size
+  // Creating a single-res TSDF map
   const Eigen::Vector3f map_dim(10.f, 10.f, 20.f);
-  const float map_res(0.02f);
+  const float           map_res(0.02f);
   se::TSDFMap<se::Res::Single> map_tsdf(map_dim, map_res);
+
+  // Setup input images
+  Eigen::Vector2i input_img_res(640, 480);
+  static se::Image<se::depth_t> input_depth_img(input_img_res.x(), input_img_res.y());
+  static se::Image<uint32_t>    input_rgba_img(input_img_res.x(), input_img_res.y());
+
+  int downsampling_factor = 2;
+
+  // Setup processed images
+  Eigen::Vector2i processed_img_res = input_img_res / downsampling_factor;
+  static se::Image<se::depth_t> processed_depth_img(processed_img_res.x(), processed_img_res.y());
+  static se::Image<uint32_t>    processed_rgba_img(processed_img_res.x(), processed_img_res.y());
+
+  // Setup output images / renders
+  static uint32_t* output_rgba_img_data     =  new uint32_t[processed_img_res.x() * processed_img_res.y()];
+  static uint32_t* output_depth_img_data    =  new uint32_t[processed_img_res.x() * processed_img_res.y()];
+  static uint32_t* output_tracking_img_data =  new uint32_t[processed_img_res.x() * processed_img_res.y()];
+  static uint32_t* output_volume_img_data   =  new uint32_t[processed_img_res.x() * processed_img_res.y()];
 
   // Setup sensor
   se::SensorConfig sensor_config;
-  int ds = 2;
-  sensor_config.width  =  640   / ds;
-  sensor_config.height =  480   / ds;
-  sensor_config.fx     =  481.2 / ds;
-  sensor_config.fy     = -480.0 / ds;
-  sensor_config.cx     =  (319.5 + 0.5f) / ds - 0.5f;
-  sensor_config.cy     =  (239.5 + 0.5f) / ds - 0.5f;
+  sensor_config.width  =  input_img_res.x();
+  sensor_config.height =  input_img_res.y();
+  sensor_config.fx     =  481.2;
+  sensor_config.fy     = -480.0;
+  sensor_config.cx     =  319.5;
+  sensor_config.cy     =  239.5;
   sensor_config.left_hand_frame = sensor_config.fy < 0;
   sensor_config.near_plane      = 0.4f;
   sensor_config.far_plane       = 6.f;
 
-  const se::PinholeCamera sensor(sensor_config);
+  // Create a pinhole camera and downsample the intrinsics
+  const se::PinholeCamera sensor(sensor_config, downsampling_factor);
 
   // Setup reader
-  static se::Reader* reader = nullptr;
-
   se::Configuration config;
   config.fps                = 24;
   config.drop_frames        = false;
@@ -53,6 +66,7 @@ int main() {
   config.ground_truth_file  = "/home/nils/workspace_/projects/supereight_2/se_2/datasets/icl_nuim/traj_2/scene.raw.txt";
 
   // ========= READER INITIALIZATION  =========
+  static se::Reader* reader = nullptr;
   reader = se::create_reader(config);
 
   if (reader == nullptr) {
@@ -62,18 +76,6 @@ int main() {
   // Setup input, processed and output imgs
   se::ReaderStatus read_ok = se::ReaderStatus::ok;
   Eigen::Matrix4f T_MS;
-  Eigen::Vector2i input_img_res(640, 480);
-  static se::Image<se::depth_t> input_depth_img(input_img_res.x(), input_img_res.y());
-  static se::Image<uint32_t> input_rgba_img(input_img_res.x(), input_img_res.y());
-
-  Eigen::Vector2i processed_img_res = input_img_res / ds;
-  static se::Image<se::depth_t> processed_depth_img(processed_img_res.x(), processed_img_res.y());
-  static se::Image<uint32_t>    processed_rgba_img(processed_img_res.x(), processed_img_res.y());
-
-  static uint32_t* output_rgba_img_data     =  new uint32_t[processed_img_res.x() * processed_img_res.y()];
-  static uint32_t* output_depth_img_data    =  new uint32_t[processed_img_res.x() * processed_img_res.y()];
-  static uint32_t* output_tracking_img_data =  new uint32_t[processed_img_res.x() * processed_img_res.y()];
-  static uint32_t* output_volume_img_data   =  new uint32_t[processed_img_res.x() * processed_img_res.y()];
 
   se::TrackerConfig tracker_config;
   tracker_config.iterations = {10, 5, 4};
