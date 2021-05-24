6492dae592a44bfd5cdf95651420d0ff9a2703ae
Nils Funk <nilsiism>
2021-05-24 12:29:11 +0100

    Add multi-res interpolation unit test

--- se_map/test/octree_visitor/octree_visitor_unittest.cpp
+++ se_map/test/octree_visitor/octree_visitor_unittest.cpp
@@ -138,3 +138,93 @@ TEST(SingleResVisitor, Interpolation)
   interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(16.f,16.f,16.f));
   EXPECT_FALSE(interp_field_value);
 }
+
+
+TEST(MultiResVisitor, Interpolation)
+{
+  Eigen::Vector3i block_coord;
+  Eigen::Vector3i coord_ought;
+  Eigen::Vector3i coord_is;
+
+  typedef se::TSDFData DataType;
+
+  se::scale_t max_tree_scale = 5;
+  unsigned int octree_size = 1 << max_tree_scale;
+
+  // SCALE 0
+  constexpr se::scale_t max_block_scale_3 = 3;
+  constexpr size_t      block_size_3      = 1 << max_block_scale_3;
+
+  typedef se::Octree<DataType, se::Res::Multi, block_size_3> OctreeType0;
+  typedef OctreeType0::BlockType BlockType0;
+
+  std::vector<Eigen::Vector3i> block_coords =
+  {
+    Eigen::Vector3i(           0,            0,            0),
+    Eigen::Vector3i(block_size_3,            0,            0),
+    Eigen::Vector3i(           0, block_size_3,            0),
+    Eigen::Vector3i(block_size_3, block_size_3,            0),
+    Eigen::Vector3i(           0,            0, block_size_3),
+    Eigen::Vector3i(block_size_3,            0, block_size_3),
+    Eigen::Vector3i(           0, block_size_3, block_size_3),
+    Eigen::Vector3i(block_size_3, block_size_3, block_size_3)
+  };
+
+  std::shared_ptr<OctreeType0> octree_ptr_0 = std::shared_ptr<OctreeType0>(new OctreeType0(octree_size));
+
+  BlockType0* block_ptr_0 = nullptr;
+
+  for (size_t i = 0; i < block_coords.size(); i++)
+  {
+    const Eigen::Vector3i block_coord = block_coords[i];
+    coord_ought = adapt_to_scale(block_coord, octree_ptr_0->max_block_scale);
+    se::key_t voxel_key;
+    se::keyops::encode_key(block_coord, 0, voxel_key);
+    block_ptr_0 = static_cast<BlockType0*>(se::allocator::block(voxel_key, *octree_ptr_0, octree_ptr_0->getRoot()));
+    coord_is = block_ptr_0->getCoord();
+    block_ptr_0->setCurrentScale(1);
+    EXPECT_EQ(coord_ought, coord_is);
+    for (size_t voxel_idx = block_ptr_0->size_cu; voxel_idx < block_ptr_0->size_cu + 64; voxel_idx++)
+    {
+      BlockType0::DataType data;
+      data.tsdf = i;
+      data.weight = 1;
+      block_ptr_0->setData(voxel_idx, data);
+    }
+  }
+
+  auto interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(2.f,2.f,2.f));
+  EXPECT_TRUE(interp_field_value);
+  EXPECT_EQ(0, *interp_field_value);
+
+  interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(8.f,2.f,2.f));
+  EXPECT_TRUE(interp_field_value);
+  EXPECT_EQ(0.5, *interp_field_value);
+
+  interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(2.f,8.f,2.f));
+  EXPECT_TRUE(interp_field_value);
+  EXPECT_EQ(1, *interp_field_value);
+
+  interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(8.f,8.f,2.f));
+  EXPECT_TRUE(interp_field_value);
+  EXPECT_EQ(1.5, *interp_field_value);
+
+  interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(2.f,2.f,8.f));
+  EXPECT_TRUE(interp_field_value);
+  EXPECT_EQ(2, *interp_field_value);
+
+  interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(8.f,2.f,8.f));
+  EXPECT_TRUE(interp_field_value);
+  EXPECT_EQ(2.5, *interp_field_value);
+
+  interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(2.f,8.f,8.f));
+  EXPECT_TRUE(interp_field_value);
+  EXPECT_EQ(3, *interp_field_value);
+
+  interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(8.f,8.f,8.f));
+  EXPECT_TRUE(interp_field_value);
+  EXPECT_EQ(3.5, *interp_field_value);
+
+  interp_field_value = se::visitor::getFieldInterp(*octree_ptr_0, Eigen::Vector3f(16.f,16.f,16.f));
+  EXPECT_FALSE(interp_field_value);
+}
