d67feccb19cf54eb01f48f142200ff40854688fb
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2021-05-27 14:40:17 +0100

    Fix the se::allocator::blocks() unit test
    The Block pointers were not necessarily returned with the same order as
    the input Morton codes since the allocation happens in parallel. Now the
    results are sorted in the unit test so that comparisons are easier and
    deterministic.

--- se_map/test/octree_allocator/octree_allocator_unittest.cpp
+++ se_map/test/octree_allocator/octree_allocator_unittest.cpp
@@ -31,6 +31,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <gtest/gtest.h>
 
+#include <algorithm>
+
 #include "se/utils/type_util.hpp"
 #include "se/utils/key_util.hpp"
 #include "se/utils/octant_util.hpp"
@@ -48,6 +50,19 @@ Eigen::Vector3i adapt_to_scale(const Eigen::Vector3i& coord, const se::scale_t s
   return adapted_coord;
 }
 
+bool vector3i_less(const Eigen::Vector3i& lhs, const Eigen::Vector3i& rhs)
+{
+  if (lhs.x() < rhs.x()) {
+    return true;
+  } else if (lhs.x() == rhs.x() && lhs.y() < rhs.y()) {
+    return true;
+  } else if (lhs.x() == rhs.x() && lhs.y() == rhs.y() && lhs.z() < rhs.z()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 
 TEST(SingleResAllocation, BlockKey)
@@ -58,7 +73,7 @@ TEST(SingleResAllocation, BlockKey)
 
   typedef se::TSDFData DataType;
   typedef se::Node<DataType, se::Res::Single> NodeType;
-  
+
   const NodeType* node_ptr = nullptr;
   se::OctantBase* octant_ptr = nullptr;
 
@@ -75,14 +90,14 @@ TEST(SingleResAllocation, BlockKey)
   // SCALE 0
   constexpr se::scale_t max_block_scale_0 = 0;
   constexpr size_t      block_size_0      = 1 << max_block_scale_0;
-  
+
   typedef se::Octree<DataType, se::Res::Single, block_size_0> OctreeType0;
   typedef OctreeType0::BlockType BlockType0;
 
   OctreeType0::Ptr octree_ptr_0 = OctreeType0::Ptr(new OctreeType0(octree_size));
 
   BlockType0* block_ptr_0 = nullptr;
-  
+
   for (const auto voxel_coord : voxel_coords)
   {
     coord_ought = adapt_to_scale(voxel_coord, octree_ptr_0->max_block_scale);
@@ -113,7 +128,7 @@ TEST(SingleResAllocation, BlockKey)
 
   typedef se::Octree<DataType, se::Res::Single, block_size_1> OctreeType1;
   typedef OctreeType1::BlockType BlockType1;
-  
+
   OctreeType1::Ptr octree_ptr_1 = OctreeType1::Ptr(new OctreeType1(octree_size));
 
   BlockType1* block_ptr_1 = nullptr;
@@ -301,60 +316,56 @@ TEST(SingleResAllocation, BlockCoord)
 
 TEST(SingleResAllocation, BlockKeys)
 {
-  se::scale_t max_tree_scale = 8;
-  unsigned int octree_size = 1 << max_tree_scale;
-
   typedef se::Octree<se::TSDFData, se::Res::Single> OctreeType;
   typedef OctreeType::NodeType NodeType;
   typedef OctreeType::BlockType BlockType;
 
+  // Create an octree.
+  constexpr se::scale_t max_tree_scale = 8;
+  constexpr int octree_size = 1 << max_tree_scale;
   OctreeType::Ptr octree_ptr = OctreeType::Ptr(new OctreeType(octree_size));
 
-  Eigen::Vector3i coord_ought;
-  Eigen::Vector3i coord_is;
-
-  const NodeType* node_ptr = nullptr;
-  se::OctantBase* octant_ptr = nullptr;
-
+  // The coordinates of some voxels.
   std::vector<Eigen::Vector3i> voxel_coords =
   {
-    Eigen::Vector3i(233,  44, 255),
+    Eigen::Vector3i( 33, 104,  55),
     Eigen::Vector3i(113, 144, 155),
-    Eigen::Vector3i( 33, 104,  55)
+    Eigen::Vector3i(233,  44, 255)
   };
+  // Sort the voxel coordinates to make later comparisons easier.
+  std::sort(voxel_coords.begin(), voxel_coords.end(), vector3i_less);
 
-  std::vector<se::key_t> voxel_keys;
-  for (const auto voxel_coord : voxel_coords)
-  {
-    se::key_t voxel_key;
-    se::keyops::encode_key(voxel_coord, 0, voxel_key);
-    voxel_keys.push_back(voxel_key);
-  }
+  // Get the coordinates of the corresponding Blocks from the voxel coordinates.
+  std::vector<Eigen::Vector3i> desired_block_coords (voxel_coords.size());
+  std::transform(voxel_coords.begin(), voxel_coords.end(), desired_block_coords.begin(),
+      [=](const Eigen::Vector3i& x) { return adapt_to_scale(x, octree_ptr->max_block_scale); });
+  // Sort the Block coordinates to make later comparisons easier.
+  std::sort(desired_block_coords.begin(), desired_block_coords.end(), vector3i_less);
 
-  se::keyops::sort_keys(voxel_keys);
+  // Allocate the Blocks.
+  std::vector<se::OctantBase*> block_ptrs = se::allocator::blocks(voxel_coords, *octree_ptr, octree_ptr->getRoot());
 
-  std::vector<se::OctantBase*> block_ptrs = se::allocator::blocks(voxel_keys, *octree_ptr, octree_ptr->getRoot());
+  // Sort the returned Blocks based on their coordinates.
+  std::sort(block_ptrs.begin(), block_ptrs.end(),
+      [](auto b1, auto b2){ return vector3i_less(b1->getCoord(), b2->getCoord()); });
 
+  EXPECT_EQ(desired_block_coords.size(), block_ptrs.size());
+
+  // Loop over all returned Blocks.
   for (se::idx_t i = 0; i < block_ptrs.size(); ++i)
   {
-    Eigen::Vector3i voxel_coord;
-    se::scale_t     voxel_scale;
-    se::keyops::decode_key(voxel_keys[i], voxel_coord, voxel_scale);
-    coord_ought = adapt_to_scale(voxel_coord, octree_ptr->max_block_scale);
-    auto block_ptr   = static_cast<BlockType*>(block_ptrs[i]);
-    coord_is = block_ptr->getCoord();
-    EXPECT_EQ(coord_ought, coord_is);
+    auto block_ptr = static_cast<BlockType*>(block_ptrs[i]);
+    const Eigen::Vector3i actual_block_coord = block_ptr->getCoord();
+    EXPECT_EQ(desired_block_coords[i], actual_block_coord);
 
-    octant_ptr = block_ptr->getParent();
+    const se::OctantBase* octant_ptr = block_ptr->getParent();
     for (se::scale_t s = 4; s <= 8; ++s)
     {
-      node_ptr = static_cast<const NodeType*>(octant_ptr);
-      coord_is    = node_ptr->getCoord();
-      coord_ought = adapt_to_scale(voxel_coord, s);
+      const NodeType* node_ptr = static_cast<const NodeType*>(octant_ptr);
+      const Eigen::Vector3i coord_is = node_ptr->getCoord();
+      const Eigen::Vector3i coord_ought = adapt_to_scale(voxel_coords[i], s);
       EXPECT_EQ(coord_ought, coord_is);
-      se::OctantBase* octant_tmp_ptr = nullptr;
-      octant_tmp_ptr = octant_ptr->getParent();;
-      octant_ptr = octant_tmp_ptr;
+      octant_ptr = octant_ptr->getParent();
     }
     EXPECT_EQ(nullptr, octant_ptr);
   }
