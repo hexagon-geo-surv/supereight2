671a61b57687a4031c4ebeaffa9501637ed5ebdb
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2021-06-09 12:01:16 +0100

    Remove duplicate code

--- se_shared/include/se/image/image.hpp
+++ se_shared/include/se/image/image.hpp
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2018 Emanuele Vespa
+// SPDX-FileCopyrightText: 2020-2021 Smart Robotics Lab
+// SPDX-FileCopyrightText: 2020-2021 Nils Funk, Imperial College London
+// SPDX-FileCopyrightText: 2020-2021 Sotiris Papatheodorou, Imperial College London
+// SPDX-License-Identifier: BSD-3-Clause
+
 #ifndef SE_IMAGE_HPP
 #define SE_IMAGE_HPP
 
@@ -44,60 +50,6 @@ class Image {
 };
 
 
-static uint32_t gray_to_rgba(double h) {
-  constexpr double v = 0.75;
-  double r = 0, g = 0, b = 0;
-  if (v > 0) {
-    constexpr double m = 0.25;
-    constexpr double sv = 0.6667;
-    h *= 6.0;
-    const int sextant = static_cast<int>(h);
-    const double fract = h - sextant;
-    const double vsf = v * sv * fract;
-    const double mid1 = m + vsf;
-    const double mid2 = v - vsf;
-    switch (sextant) {
-      case 0:
-        r = v;
-        g = mid1;
-        b = m;
-        break;
-      case 1:
-        r = mid2;
-        g = v;
-        b = m;
-        break;
-      case 2:
-        r = m;
-        g = v;
-        b = mid1;
-        break;
-      case 3:
-        r = m;
-        g = mid2;
-        b = v;
-        break;
-      case 4:
-        r = mid1;
-        g = m;
-        b = v;
-        break;
-      case 5:
-        r = v;
-        g = m;
-        b = mid2;
-        break;
-      default:
-        r = 0;
-        g = 0;
-        b = 0;
-        break;
-    }
-  }
-  return se::pack_rgba(r * 255, g * 255, b * 255, 255);
-}
-
-
 
 static void convert_to_output_rgba_img(const se::Image<uint32_t>& input_rgba_img,
                                        uint32_t*                  output_rgba_img_data)
@@ -113,22 +65,8 @@ static void convert_to_output_depth_img(const se::Image<float>& input_depth_img,
 {
   const float min_depth = 0.4f;
   const float max_depth = 6.0f;
-
-  const float range_scale = 1.0f / (max_depth - min_depth);
-  for (int y = 0; y < input_depth_img.height(); y++) {
-    const int row_offset = y * input_depth_img.width();
-    for (int x = 0; x < input_depth_img.width(); x++) {
-      const int pixel_idx = row_offset + x;
-      if (input_depth_img[pixel_idx] < min_depth) {
-        output_depth_img_data[pixel_idx] = 0xFFFFFFFF; // White
-      } else if (input_depth_img[pixel_idx] > max_depth) {
-        output_depth_img_data[pixel_idx] = 0xFF000000; // Black
-      } else {
-        const float depth_value = (input_depth_img[pixel_idx] - min_depth) * range_scale;
-        output_depth_img_data[pixel_idx] = gray_to_rgba(depth_value);
-      }
-    }
-  }
+  depth_to_rgba(output_depth_img_data, input_depth_img.data(),
+      Eigen::Vector2i(input_depth_img.width(), input_depth_img.height()), min_depth, max_depth);
 }
 
 
