398d8abf689b426f465b01a8bafdbf93399cfd71
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2022-03-16 21:23:12 +0000

    Simplify create_reader()

--- app/src/reader.cpp
+++ app/src/reader.cpp
@@ -1,9 +1,9 @@
 /*
  * SPDX-FileCopyrightText: 2014 University of Edinburgh, Imperial College London, University of Manchester
  * SPDX-FileCopyrightText: 2016-2019 Emanuele Vespa
- * SPDX-FileCopyrightText: 2020-2021 Smart Robotics Lab, Imperial College London, Technical University of Munich
+ * SPDX-FileCopyrightText: 2020-2022 Smart Robotics Lab, Imperial College London, Technical University of Munich
  * SPDX-FileCopyrightText: 2020-2021 Nils Funk
- * SPDX-FileCopyrightText: 2020-2021 Sotiris Papatheodorou
+ * SPDX-FileCopyrightText: 2020-2022 Sotiris Papatheodorou
  * SPDX-License-Identifier: MIT
  */
 
@@ -22,28 +22,27 @@
 se::Reader* se::create_reader(const se::ReaderConfig& config)
 {
     se::Reader* reader = nullptr;
-    if (config.reader_type == se::ReaderType::OPENNI) {
+    switch (config.reader_type) {
+    case se::ReaderType::OPENNI:
         reader = new se::OpenNIReader(config);
-    }
-    else if (config.reader_type == se::ReaderType::RAW) {
+        break;
+    case se::ReaderType::RAW:
         reader = new se::RAWReader(config);
-    }
-    else if (config.reader_type == se::ReaderType::NEWERCOLLEGE) {
+        break;
+    case se::ReaderType::NEWERCOLLEGE:
         reader = new se::NewerCollegeReader(config);
-    }
-    else if (config.reader_type == se::ReaderType::TUM) {
+        break;
+    case se::ReaderType::TUM:
         reader = new se::TUMReader(config);
-    }
-    else if (config.reader_type == se::ReaderType::INTERIORNET) {
+        break;
+    case se::ReaderType::INTERIORNET:
         reader = new se::InteriorNetReader(config);
-    }
-    else {
+        break;
+    default:
         std::cerr << "Error: Unrecognised file format, file not loaded\n";
-        reader = nullptr;
     }
-
     // Handle failed initialization
-    if ((reader != nullptr) && !reader->good()) {
+    if (reader && !reader->good()) {
         delete reader;
         reader = nullptr;
     }
