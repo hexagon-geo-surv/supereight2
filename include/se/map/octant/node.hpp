5f42a97e11596d6f5cb6e44e114f7552ac59ee26
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2022-12-07 19:46:23 +0000

    Add se::Node::getChildCoord()

--- include/se/map/octant/node.hpp
+++ include/se/map/octant/node.hpp
@@ -240,6 +240,9 @@ class Node : public OctantBase,
      */
     OctantBase* setChild(const int child_idx, OctantBase* child_ptr);
 
+    /** Return the coordinates in voxels of the child with index \p child_idx. */
+    Eigen::Vector3i getChildCoord(const int child_idx) const;
+
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
     private:
