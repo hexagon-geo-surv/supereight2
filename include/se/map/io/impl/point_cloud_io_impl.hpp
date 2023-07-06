d0e66ed863c9390e83275455b2bfaf33ce7525bd
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2023-07-06 21:03:09 +0200

    Fix point cloud scaling when saved to VTK

--- include/se/map/io/impl/point_cloud_io_impl.hpp
+++ include/se/map/io/impl/point_cloud_io_impl.hpp
@@ -29,8 +29,7 @@ int save_point_cloud_vtk(se::Image<Eigen::Vector3f>& point_cloud,
     // Write the point data.
     for (size_t i = 0; i < point_cloud.size(); ++i) {
         const Eigen::Vector3f point_W = (T_WC * point_cloud[i].homogeneous()).head(3);
-        file << (point_W.x() + 3) / 0.04 << " " << (point_W.y() + 3) / 0.04 << " "
-             << (point_W.z() + 3) / 0.04 << "\n";
+        file << point_W.x() << " " << point_W.y() << " " << point_W.z() << "\n";
     }
 
     file.close();
