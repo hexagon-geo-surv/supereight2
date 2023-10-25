e9ea36af6c0107fb79be6fcbf04af6bfe5d065ff
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2023-10-25 14:10:27 +0200

    Add helper functions for Eigen objects

---
+++ include/se/common/eigen_utils.hpp
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Smart Robotics Lab, Imperial College London, Technical University of Munich
+ * SPDX-FileCopyrightText: 2023 Sotiris Papatheodorou
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SE_COMMON_EIGEN_UTILS_HPP
+#define SE_COMMON_EIGEN_UTILS_HPP
+
+#include <Eigen/Geometry>
+
+namespace se {
+/** Helper functions for Eigen objects. */
+namespace eigen {
+
+/** Morphologically dilate or erode an axis aligned \p box by some \p distance. If \p distance is
+ * positive then \p box will be dilated (increasing its volume), otherwise it will be eroded
+ * (decreasing its volume).
+ *
+ * \warning Erosion may result in an empty axis aligned box. Make sure to test with
+ * Eigen::AlignedBox::isEmpty() before using it to avoid undefined behavior.
+ */
+template<typename ScalarT, int Dim>
+Eigen::AlignedBox<ScalarT, Dim> dilate_erode(const Eigen::AlignedBox<ScalarT, Dim>& box,
+                                             const ScalarT distance);
+
+/** Transform a 3D axis aligned box \p box_A expressed in frame A to frame B using the transform \p
+ * T_BA.
+ */
+template<typename ScalarT, int Mode, int Options>
+Eigen::AlignedBox<ScalarT, 3> transform(const Eigen::Transform<ScalarT, 3, Mode, Options>& T_BA,
+                                        const Eigen::AlignedBox<ScalarT, 3>& box_A);
+
+} // namespace eigen
+} // namespace se
+
+#include "impl/eigen_utils_impl.hpp"
+
+#endif // SE_COMMON_EIGEN_UTILS_HPP
---
+++ include/se/common/impl/eigen_utils_impl.hpp
@@ -0,0 +1,55 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Smart Robotics Lab, Imperial College London, Technical University of Munich
+ * SPDX-FileCopyrightText: 2023 Sotiris Papatheodorou
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SE_COMMON_EIGEN_UTILS_IMPL_HPP
+#define SE_COMMON_EIGEN_UTILS_IMPL_HPP
+
+namespace se {
+namespace eigen {
+
+template<typename ScalarT, int Dim>
+Eigen::AlignedBox<ScalarT, Dim> dilate_erode(const Eigen::AlignedBox<ScalarT, Dim>& box,
+                                             const ScalarT distance)
+{
+    if (box.isEmpty()) {
+        return box;
+    }
+    return Eigen::AlignedBox<ScalarT, Dim>(box.min().array() - distance,
+                                           box.max().array() + distance);
+}
+
+
+
+template<typename ScalarT, int Mode, int Options>
+Eigen::AlignedBox<ScalarT, 3> transform(const Eigen::Transform<ScalarT, 3, Mode, Options>& T_BA,
+                                        const Eigen::AlignedBox<ScalarT, 3>& box_A)
+{
+    if (box_A.isEmpty()) {
+        // Empty aligned boxes are the same in all frames.
+        return box_A;
+    }
+    // Transform the corners of box_A to frame B and compute their AABB. There's no other way to
+    // compute the axis-aligned box in frame B since T_BA may contain a rotation.
+    // clang-format off
+    const Eigen::Matrix<ScalarT, 3, 8> corners_A = (Eigen::Matrix<ScalarT, 3, 8>() <<
+        box_A.corner(Eigen::AlignedBox<ScalarT, 3>::CornerType::BottomLeftFloor),
+        box_A.corner(Eigen::AlignedBox<ScalarT, 3>::CornerType::BottomRightFloor),
+        box_A.corner(Eigen::AlignedBox<ScalarT, 3>::CornerType::TopLeftFloor),
+        box_A.corner(Eigen::AlignedBox<ScalarT, 3>::CornerType::TopRightFloor),
+        box_A.corner(Eigen::AlignedBox<ScalarT, 3>::CornerType::BottomLeftCeil),
+        box_A.corner(Eigen::AlignedBox<ScalarT, 3>::CornerType::BottomRightCeil),
+        box_A.corner(Eigen::AlignedBox<ScalarT, 3>::CornerType::TopLeftCeil),
+        box_A.corner(Eigen::AlignedBox<ScalarT, 3>::CornerType::TopRightCeil)).finished();
+    // clang-format on
+    const Eigen::Matrix<ScalarT, 3, 8> corners_B = T_BA * corners_A;
+    return Eigen::AlignedBox<ScalarT, 3>(corners_B.rowwise().minCoeff(),
+                                         corners_B.rowwise().maxCoeff());
+}
+
+} // namespace eigen
+} // namespace se
+
+#endif // SE_COMMON_EIGEN_UTILS_IMPL_HPP
