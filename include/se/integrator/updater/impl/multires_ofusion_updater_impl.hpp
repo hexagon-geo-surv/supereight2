hexagon-geo-surv/supereight2

commit b06220b89c4f2d3a6865af738844e7631c481d56

Fix stale pointers in updated octants
Due to the pruning performed during occupancy mapping up-propagation,
the returned updated octants could include stale pointers to now freed
octants.

This fix has the positive side effect of keeping track of the actually
updated octants instead of just the allocated ones.

The fix results in a slight decrease in performance as seen in the
benchmark results with a 1 cm map in the ICL-NUIM living room 2 and TUM
RGB-D fr1_desk sequences:

ICL-NUIM living_room_traj2_frei_png 1 cm
commit    octant-tracking   update (s)   memory (MB)
this      no                0.044158     3916.167101
this      yes               0.045367     3917.055947
v0.18.0   no                0.042369     3916.214812
v0.18.0   yes               0.042947     3916.309671

TUM RGB-D rgbd_dataset_freiburg1_desk 1 cm
commit    octant-tracking   update (s)   memory (MB)
this      no                0.007728     446.572122
this      yes               0.008525     447.019472
v0.18.0   no                0.007673     446.743656
v0.18.0   yes               0.007549     446.715039

where octant-tracking refers to whether the updated_octants argument was
passed to integratedDepth() or not.

include/se/integrator/updater/impl/multires_ofusion_updater_impl.hpp
@@ -38,7 +38,8 @@ Updater<Map<Data<Field::Occupancy, ColB, SemB>, Res::Multi, BlockSize>, SensorT>
 
 template<Colour ColB, Semantics SemB, int BlockSize, typename SensorT>
 void Updater<Map<Data<Field::Occupancy, ColB, SemB>, Res::Multi, BlockSize>, SensorT>::operator()(
-    VolumeCarverAllocation& allocation_list)
+    VolumeCarverAllocation& allocation_list,
+    std::vector<const OctantBase*>* updated_octants)
 {
     TICK("fusion-total")
 
@@ -76,11 +77,28 @@ void Updater<Map<Data<Field::Occupancy, ColB, SemB>, Res::Multi, BlockSize>, Sen
     }
     TOCK("propagation-blocks")
 
+    // updated_octants_ must be populated with blocks before the call to propagateToRoot because it
+    // may prune octants.
+    track_updated_octants_ = updated_octants;
+    if (track_updated_octants_) {
+        updated_octants_.clear();
+        updated_octants_.insert(allocation_list.block_list.begin(),
+                                allocation_list.block_list.end());
+        updated_octants_.insert(freed_block_list_.begin(), freed_block_list_.end());
+    }
+
     TICK("propagation-to-root")
     propagateToRoot(allocation_list.block_list);
     TOCK("propagation-to-root")
 
     TOCK("propagation-total")
+
+    if (track_updated_octants_) {
+        updated_octants->clear();
+        updated_octants->reserve(updated_octants_.size());
+        updated_octants->insert(
+            updated_octants->end(), updated_octants_.begin(), updated_octants_.end());
+    }
 }
 
 
@@ -108,10 +126,22 @@ void Updater<Map<Data<Field::Occupancy, ColB, SemB>, Res::Multi, BlockSize>,
                 auto node_data =
                     updater::propagate_to_parent_node<NodeType, BlockType>(octant_ptr, frame_);
                 node_set_[d - 1].insert(octant_ptr->getParent());
+                if (track_updated_octants_) {
+                    updated_octants_.insert(octant_ptr);
+                }
 
                 if (node_data.observed
                     && get_field(node_data) <= 0.95 * MapType::DataType::min_occupancy) {
-                    octree_.deleteChildren(static_cast<NodeType*>(octant_ptr));
+                    auto* node_ptr = static_cast<NodeType*>(octant_ptr);
+                    if (track_updated_octants_) {
+                        for (int i = 0; i < 8; i++) {
+                            OctantBase* const child_ptr = node_ptr->getChild(i);
+                            if (child_ptr) {
+                                updated_octants_.erase(child_ptr);
+                            }
+                        }
+                    }
+                    octree_.deleteChildren(node_ptr);
                 }
 
             } // if parent

include/se/integrator/impl/map_integrator_impl.hpp
@@ -266,22 +266,8 @@ void IntegrateDepthImplD<se::Field::Occupancy, se::Res::Multi>::integrate(
     // Update
     TICK("update")
     se::Updater updater(map, sensor, depth_img, T_WS, frame);
-    updater(allocation_list);
+    updater(allocation_list, updated_octants);
     TOCK("update")
-
-    if (updated_octants) {
-        // TODO: Currently returning allocated, not updated octants. Remove non-updated octants from
-        // allocation_list during the call to se::Updater::update() to return only updated octants.
-        updated_octants->clear();
-        updated_octants->reserve(allocation_list.node_list.size()
-                                 + allocation_list.block_list.size());
-        updated_octants->insert(updated_octants->end(),
-                                allocation_list.node_list.begin(),
-                                allocation_list.node_list.end());
-        updated_octants->insert(updated_octants->end(),
-                                allocation_list.block_list.begin(),
-                                allocation_list.block_list.end());
-    }
 }
 
 

include/se/integrator/updater/multires_ofusion_updater.hpp
@@ -10,6 +10,8 @@
 
 
 
+#include <set>
+
 #include "se/sensor/sensor.hpp"
 
 
@@ -57,7 +59,8 @@ class Updater<Map<Data<Field::Occupancy, ColB, SemB>, Res::Multi, BlockSize>, Se
             const Eigen::Matrix4f& T_WS,
             const int frame);
 
-    void operator()(VolumeCarverAllocation& allocation_list);
+    void operator()(VolumeCarverAllocation& allocation_list,
+                    std::vector<const OctantBase*>* updated_octants = nullptr);
 
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
@@ -97,6 +100,8 @@ class Updater<Map<Data<Field::Occupancy, ColB, SemB>, Res::Multi, BlockSize>, Se
     const UpdaterConfig config_;
     std::vector<std::set<OctantBase*>> node_set_;
     std::vector<OctantBase*> freed_block_list_;
+    std::set<OctantBase*> updated_octants_;
+    bool track_updated_octants_ = false;
 };
 
 

commit 10d8019c8dddb005922b869c1707b24cb3e96c9d

Fix omitting leaf nodes in updated octants
The issue was introduced in commit b06220b. Only the nodes traversed
during up-propagation from blocks were tracked resulting in leaf nodes
being skipped.

The parent's of leaf nodes aren't added to updated_octants_ but they
also don't seem to be updated. Up-propagation seems to only happen for
blocks and not for leaf nodes.

include/se/integrator/updater/impl/multires_ofusion_updater_impl.hpp
@@ -77,14 +77,21 @@ void Updater<Map<Data<Field::Occupancy, ColB, SemB>, Res::Multi, BlockSize>, Sen
     }
     TOCK("propagation-blocks")
 
-    // updated_octants_ must be populated with blocks before the call to propagateToRoot because it
-    // may prune octants.
+    // updated_octants_ must be populated with all potentially updated octants (blocks and leaf
+    // nodes) before the call to propagateToRoot(). propagateToRoot() may prune octants, thus
+    // deallocating blocks and removing elements from updated_octants_, so this prevents having
+    // stale pointers in updated_octants_. Leaf nodes won't be traversed in propagateToRoot() so
+    // they have to be added as well.
     track_updated_octants_ = updated_octants;
     if (track_updated_octants_) {
         updated_octants_.clear();
         updated_octants_.insert(allocation_list.block_list.begin(),
                                 allocation_list.block_list.end());
         updated_octants_.insert(freed_block_list_.begin(), freed_block_list_.end());
+        std::copy_if(allocation_list.node_list.begin(),
+                     allocation_list.node_list.end(),
+                     std::inserter(updated_octants_, updated_octants_.end()),
+                     [](const auto octant) { return octant && octant->isLeaf(); });
     }
 
     TICK("propagation-to-root")