46c5107e15f9b314c64927b9dd95c9835a2ce7b5
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2024-06-12 20:31:17 +0200

    Simplify depth_to_rgba()

--- src/common/colour_utils.cpp
+++ src/common/colour_utils.cpp
@@ -16,27 +16,24 @@ void se::depth_to_rgba(se::RGBA* depth_RGBA_image_data,
                        const float max_depth)
 {
     const float inv_depth_range = 1.0f / (max_depth - min_depth);
+    const size_t num_pixels = size_t(depth_image_res.x()) * size_t(depth_image_res.y());
 #pragma omp parallel for
-    for (int y = 0; y < depth_image_res.y(); y++) {
-        const int row_offset = y * depth_image_res.x();
-        for (int x = 0; x < depth_image_res.x(); x++) {
-            const int pixel_idx = x + row_offset;
-            const float depth = depth_image_data[pixel_idx];
-            if (depth <= 0.0f || std::isnan(depth)) {
-                depth_RGBA_image_data[pixel_idx] = {0x00, 0x00, 0x00, 0xFF}; // Black
-            }
-            else if (depth < min_depth) {
-                depth_RGBA_image_data[pixel_idx] = {0x80, 0x80, 0x80, 0xFF}; // Gray
-            }
-            else if (depth > max_depth) {
-                depth_RGBA_image_data[pixel_idx] = {0xFF, 0xFF, 0xFF, 0xFF}; // White
-            }
-            else {
-                const float normalized_depth = (depth - min_depth) * inv_depth_range;
-                const tinycolormap::Color c = tinycolormap::GetColor(
-                    1.0f - normalized_depth, tinycolormap::ColormapType::Heat);
-                depth_RGBA_image_data[pixel_idx] = {c.ri(), c.gi(), c.bi()};
-            }
+    for (size_t i = 0; i < num_pixels; i++) {
+        const float depth = depth_image_data[i];
+        if (depth <= 0.0f || std::isnan(depth)) {
+            depth_RGBA_image_data[i] = {0x00, 0x00, 0x00}; // Black
+        }
+        else if (depth < min_depth) {
+            depth_RGBA_image_data[i] = {0x80, 0x80, 0x80}; // Gray
+        }
+        else if (depth > max_depth) {
+            depth_RGBA_image_data[i] = {0xFF, 0xFF, 0xFF}; // White
+        }
+        else {
+            const float normalized_depth = (depth - min_depth) * inv_depth_range;
+            const tinycolormap::Color c =
+                tinycolormap::GetColor(1.0f - normalized_depth, tinycolormap::ColormapType::Heat);
+            depth_RGBA_image_data[i] = {c.ri(), c.gi(), c.bi()};
         }
     }
 }
