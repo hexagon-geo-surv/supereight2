76e338b8071b1c28a6605de8a060cc22817d2ab6
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2024-06-12 19:20:23 +0200

    Use tinycolormap for depth image rendering

--- src/common/colour_utils.cpp
+++ src/common/colour_utils.cpp
@@ -7,66 +7,7 @@
 
 #include "se/common/colour_utils.hpp"
 
-
-
-se::RGBA gray_to_rgba(double h)
-{
-    constexpr double v = 0.75;
-    double r = 0, g = 0, b = 0;
-    if (v > 0) {
-        constexpr double m = 0.25;
-        constexpr double sv = 0.6667;
-        h *= 6.0;
-        const int sextant = static_cast<int>(h);
-        const double fract = h - sextant;
-        const double vsf = v * sv * fract;
-        const double mid1 = m + vsf;
-        const double mid2 = v - vsf;
-        switch (sextant) {
-        case 0:
-            r = v;
-            g = mid1;
-            b = m;
-            break;
-        case 1:
-            r = mid2;
-            g = v;
-            b = m;
-            break;
-        case 2:
-            r = m;
-            g = v;
-            b = mid1;
-            break;
-        case 3:
-            r = m;
-            g = mid2;
-            b = v;
-            break;
-        case 4:
-            r = mid1;
-            g = m;
-            b = v;
-            break;
-        case 5:
-            r = v;
-            g = m;
-            b = mid2;
-            break;
-        default:
-            r = 0;
-            g = 0;
-            b = 0;
-            break;
-        }
-    }
-    return se::RGBA{static_cast<std::uint8_t>(r * 0xFF),
-                    static_cast<std::uint8_t>(g * 0xFF),
-                    static_cast<std::uint8_t>(b * 0xFF),
-                    0xFF};
-}
-
-
+#include <se/external/tinycolormap.hpp>
 
 void se::depth_to_rgba(se::RGBA* depth_RGBA_image_data,
                        const float* depth_image_data,
@@ -92,7 +33,9 @@ void se::depth_to_rgba(se::RGBA* depth_RGBA_image_data,
             }
             else {
                 const float normalized_depth = (depth - min_depth) * inv_depth_range;
-                depth_RGBA_image_data[pixel_idx] = gray_to_rgba(normalized_depth);
+                const tinycolormap::Color c = tinycolormap::GetColor(
+                    1.0f - normalized_depth, tinycolormap::ColormapType::Heat);
+                depth_RGBA_image_data[pixel_idx] = {c.ri(), c.gi(), c.bi()};
             }
         }
     }
