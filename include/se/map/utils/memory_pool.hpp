fc469cfab6cee656a2bc92157679dda42b7b6f25
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2024-10-30 14:33:06 +0200

    Update MemoryPool documentation

--- include/se/map/utils/memory_pool.hpp
+++ include/se/map/utils/memory_pool.hpp
@@ -14,8 +14,15 @@
 
 namespace se {
 
-/** Manages memory for octree nodes and blocks in an efficient manner. Never deallocate nodes/blocks
- * created through se::MemoryPool using \p delete.
+/** Manages memory for se::Octree nodes and blocks in an efficient manner. The destructors of all
+ * still allocated nodes and blocks are called on destruction of the se::MemoryPool object. User
+ * code shouldn't use se::MemoryPool directly as all usage is internal in se::Octree.
+ *
+ * \warning Never deallocate nodes/blocks created through se::MemoryPool using \p delete.
+ *
+ * \note Since se::MemoryPool does its own memory management on top of large heap allocations, it
+ * can mask certain use-after-free or out-of-bounds-access bugs and make them undetectable to the
+ * address sanitizer (ASAN). If you suspect such issues, use se::NullMemoryPool instead.
  */
 template<typename NodeT, typename BlockT>
 class MemoryPool {
@@ -28,13 +35,8 @@ class MemoryPool {
         return new (node_buffer_.malloc()) NodeT(coord, size, typename NodeT::DataType());
     }
 
-    /** Allocate a node.
-     *
-     * \param[in] parent_ptr The pointer to the parent of the node to be allocated.
-     * \param[in] child_idx  The child index of the node to be allocated.
-     * \param[in] init_data  The data to initialise the node with.
-     *
-     * \return The pointer to the allocated node.
+    /** Return a pointer to a newly allocated node that is the child with index \p child_idx of \p
+     * parent_ptr and initialize it with \p init_data.
      */
     NodeT* allocateNode(NodeT* const parent_ptr,
                         const int child_idx,
@@ -43,13 +45,8 @@ class MemoryPool {
         return new (node_buffer_.malloc()) NodeT(parent_ptr, child_idx, init_data);
     }
 
-    /** Allocate a block.
-     *
-     * \param[in] parent_ptr The pointer to the parent of the block to be allocated.
-     * \param[in] child_idx  The child index of the block to be allocated.
-     * \param[in] init_data  The data to initialise the block with.
-     *
-     * \return The pointer to the allocated block.
+    /** Return a pointer to a newly allocated block that is the child with index \p child_idx of \p
+     * parent_ptr and initialise it with \p init_data.
      */
     BlockT* allocateBlock(NodeT* const parent_ptr,
                           const int child_idx,
@@ -58,15 +55,13 @@ class MemoryPool {
         return new (block_buffer_.malloc()) BlockT(parent_ptr, child_idx, init_data);
     }
 
-    /** Destruct and deallocate the node pointed to by \p node_ptr.
-     */
+    /** Destruct and deallocate the node pointed to by \p node_ptr. */
     void deleteNode(NodeT* const node_ptr)
     {
         node_buffer_.destroy(node_ptr);
     }
 
-    /** Destruct and deallocate the block pointed to by \p block_ptr.
-     */
+    /** Destruct and deallocate the block pointed to by \p block_ptr. */
     void deleteBlock(BlockT* const block_ptr)
     {
         block_buffer_.destroy(block_ptr);
